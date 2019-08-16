b387c87715016659bc1fae24ff0f875e015cb3cf
jerryHZLiang <798895110@qq.com>
2019-08-16 10:02:37 +0800

    实现点击add增加list

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,20 +1,36 @@
 import React from 'react';
 
 class TodoList extends React.Component {
-    handleBtnClick() {
-        alert('c');
+    constructor(props){
+        super(props);
+        this.state = {
+            list: [],
+            inputValue: ''
+        }
     }
 
+     handleBtnClick() {
+        this.setState({
+                list: [...this.state.list, this.state.inputValue],
+            }
+        )
+    }
+     handleInputChange(e){
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
     render() {
         return (
             <div className="App">
                 <div>
-                    <input/>
-                    <button onClick={this.handleBtnClick}>add</button>
+                    <input onChange={this.handleInputChange.bind(this)}/>
+                    <button onClick={this.handleBtnClick.bind(this)}>add</button>
                 </div>
                 <ul>
-                    <li>learn react</li>
-                    <li>learn English</li>
+                    {this.state.list.map((item, index)=>{
+                        return <li key={index}>{item}</li>
+                    })}
                 </ul>
             </div>
         );
